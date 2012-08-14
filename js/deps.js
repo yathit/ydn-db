e5bdec24f95ee1ed531024b27feabfec97ae5700
mbikt <kyaw@blueprint.org>
2012-08-14 14:52:22 +0800

    transport refactoring

--- js/deps.js
+++ js/deps.js
@@ -3,7 +3,7 @@ goog.addDependency("../../../ydn-db/js/main.js", [], ['goog.debug.Console', 'goo
 goog.addDependency("../../../ydn-db/js/ydn/db/db.js", ['ydn.db.Db'], ['goog.async.Deferred', 'ydn.db.DatabaseSchema', 'ydn.db.Query']);
 goog.addDependency("../../../ydn-db/js/ydn/db/html5db.js", ['ydn.db.Html5Db'], ['ydn.db.MemoryStore']);
 goog.addDependency("../../../ydn-db/js/ydn/db/indexed_db.js", ['ydn.db.IndexedDb'], ['goog.Timer', 'goog.async.DeferredList', 'goog.events', 'ydn.async', 'ydn.db.DatabaseSchema', 'ydn.db.Db', 'ydn.db.Query', 'ydn.json']);
-goog.addDependency("../../../ydn-db/js/ydn/db/memory_store.js", ['ydn.db.MemoryStore'], ['goog.asserts', 'goog.async.Deferred']);
+goog.addDependency("../../../ydn-db/js/ydn/db/memory_store.js", ['ydn.db.MemoryStore'], ['goog.asserts', 'goog.async.Deferred', 'goog.Timer']);
 goog.addDependency("../../../ydn-db/js/ydn/db/query.js", ['ydn.db.Query'], []);
 goog.addDependency("../../../ydn-db/js/ydn/db/schema.js", ['ydn.db.DataType', 'ydn.db.DatabaseSchema', 'ydn.db.IndexSchema', 'ydn.db.StoreSchema'], []);
 goog.addDependency("../../../ydn-db/js/ydn/db/storage.js", ['ydn.db.Storage'], ['goog.userAgent.product', 'ydn.async', 'ydn.db.Html5Db', 'ydn.db.IndexedDb', 'ydn.db.MemoryStore', 'ydn.db.WebSql', 'ydn.object']);
