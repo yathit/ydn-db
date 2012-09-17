4bacb3f58a1ceafacf0820a94f3b54f611f0c86a
mbikt <kyaw@blueprint.org>
2012-09-17 10:35:40 +0800

    doc update for login system

--- test/players.js
+++ test/players.js
@@ -88,33 +88,55 @@ print_player = function(obj) {
 
 
 
-var log_them = function(pid, sno) {
-  db.get('player', pid).done(function(player) {
-    console.log([sno, 'player', player]);
-    db.get('weapon', player.weapon).done(function(weapon) {
-      console.log([sno, 'weapon', weapon]);
-    });
-  })
+var log_them = function(pid, sno, tdb) {
+  tdb = tdb || db;
+  db.getItem('current_weapon').done(function(wp) {console.log(sno + ': current_weapon: ' + wp)});
+  tdb.transaction(function tx_log(idb) {
+    idb.get('player', pid).done(function(player) {
+      console.log(sno + ': player weapon: ' + player.weapon);
+      idb.get('weapon', player.weapon).done(function(weapon) {
+        console.log(sno + ': weapon ' + weapon.name + ' count: ' + weapon.count);
+      });
+    })
+  }, ['player', 'weapon'], 'readonly');
+  console.log(sno + ': out.');
 };
-var change_weapon = function (pid, new_weapon_name, callback) {
+var change_weapon = function (pid, new_weapon_name) {
   db.transaction(function tx_change(idb) {
-    console.log('entering transaction');
-    var get_ini_data = idb.get([idb.key('player', pid), idb.key('weapon', new_weapon_name)]);
-    get_ini_data.done(function get_pre_data(data) {
-      console.log('player and new weapon data read');
-      var player = data[0];
-      var new_weapon = data[1];
-      idb.get('weapon', player.weapon).done(function (old_weapon) {
-        console.log('Changing from ' + old_weapon.name + ' to ' + new_weapon.name);
-        new_weapon.count--;
-        old_weapon.count++;
-        player.weapon = new_weapon.name;
-        idb.put('weapon', [new_weapon, old_weapon]);
-        idb.put('player', player).done(function() {
-          console.log('transaction done.');
-          callback();
+    console.log('Transaction started.');
+    var get_player_data = idb.get('player', pid);
+    get_player_data.done(function get_pre_data(player) {
+      var old_weapon_name = player.weapon;
+      db.setItem('current_weapon', old_weapon_name).done(
+        function(x) {console.log('current_weapon updated to ' + old_weapon_name)});
+      console.log('old_weapon_name: ' + old_weapon_name);
+      log_them(pid, 3, idb);
+      log_them(pid, 4);
+      idb.transaction(function tx_readwrite(idb) {
+        var get_all_data = idb.get([idb.key('player', pid), idb.key('weapon', new_weapon_name), idb.key('weapon', old_weapon_name)]);
+        get_all_data.done(function (data) {
+          var player = data[0];
+          var new_weapon = data[1];
+          idb.get('weapon', player.weapon).done(function (old_weapon) {
+            idb.setItem('current_weapon', old_weapon.name + '->' + new_weapon.name).done(
+              function(x) {console.log('current_weapon updating to ' + new_weapon.name)});
+            console.log('Changing from ' + old_weapon.name + ' to ' + new_weapon.name);
+            new_weapon.count--;
+            old_weapon.count++;
+            player.weapon = new_weapon.name;
+            log_them(pid, 5, idb);
+            log_them(pid, 6);
+            idb.put('weapon', [new_weapon, old_weapon]);
+            idb.put('player', player).done(function () {
+              idb.setItem('current_weapon', new_weapon.name).done(
+                function(x) {console.log('current_weapon updated to ' + new_weapon.name)});
+              console.log('transaction done.');
+              log_them(pid, 7);
+            });
+          })
         });
-      })
+      }, ['player', 'weapon'], 'readwrite');
     });
-  }, ['player', 'weapon'], 'readwrite');
+  }, ['player', 'weapon'], 'readonly');
+  console.log('change_weapon out.')
 };
