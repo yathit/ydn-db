bbcdde0b546ea53751fc90f80569ebdd2c3f51a5
mbikt <kyaw@blueprint.org>
2012-10-05 16:21:24 +0800

    qunit test added.

--- test/qunit/test_qunit.js
+++ test/qunit/test_qunit.js
@@ -1,11 +1,14 @@
-var db_name = "qunit_test_5";
+var db_name = "qunit_test_6";
 var store_name = "ts";    // in-line key store
 var store_name_2 = "ts2"; // out-of-line key store
 var store_name_3 = "ts3"; // in-line key + auto
 var store_name_4 = "ts4"; // out-of-line key + auto
+var store_name_5 = "ts5"; // nested keyPath
 var index_name = "ix1";
 var data_1 = { test:"test value", name:"name 1", id:1 };
+var data_1a = { test:"test value", name:"name 1", id: ['a', 'b']};
 var data_2 = { test:"test value", name:"name 2" };
+var gdata_1 = { test:"test value", name:"name 3", id: {$t: 1} };
 var msg_fail = "Creating initial situation failed";
 
 var schema = {
@@ -26,6 +29,10 @@ var schema = {
     {
       name: store_name_4,
       autoIncrement: true,
+      type: 'NUMERIC'},
+    {
+      name: store_name_5,
+      keyPath: 'id.$t', // gdata style key.
       type: 'NUMERIC'}
   ]
 };
@@ -39,7 +46,7 @@ var initionalizeDB = function(callback, opt_schema) {
   }, 100);
 };
 
-ydn.db.log('ydn.db', 300);
+// ydn.db.log('ydn.db', 300);
 
 
 module("Put", {
@@ -49,11 +56,10 @@ module("Put", {
 });
 
 
-
-asyncTest("Inserting data", function () {
+asyncTest("Put data", function () {
   expect(1);
 
-  var db = new ydn.db.Storage(db_name + '1', schema);
+  var db = new ydn.db.Storage(db_name, schema);
 
   db.put(store_name, data_1).then(function () {
     ok(true, "data inserted");
@@ -65,8 +71,26 @@ asyncTest("Inserting data", function () {
 
 });
 
-asyncTest("Inserting data with off-line-key", function () {
-  var db = new ydn.db.Storage(db_name + '2', schema);
+
+asyncTest("Put data of array key", function () {
+  expect(2);
+
+  var db = new ydn.db.Storage(db_name, schema);
+
+  db.put(store_name, data_1a).then(function (x) {
+    ok('length' in x, "array key");
+    deepEqual(data_1a.id, x, 'same key');
+    start();
+  }, function (e) {
+    ok(false, e.message);
+    start();
+  });
+
+});
+
+
+asyncTest("Put data with off-line-key", function () {
+  var db = new ydn.db.Storage(db_name, schema);
   expect(2);
 
   var key = Math.random();
@@ -81,10 +105,9 @@ asyncTest("Inserting data with off-line-key", function () {
 
 });
 
-asyncTest("Inserting data - inline-key autoincrement", function () {
-  var db = new ydn.db.Storage(db_name + '3', schema);
+asyncTest("Put data - inline-key autoincrement", function () {
+  var db = new ydn.db.Storage(db_name, schema);
   expect(2);
-
 
   db.put(store_name_3, data_1).then(function (x) {
     equal(data_1.id, x, 'key');
@@ -102,10 +125,9 @@ asyncTest("Inserting data - inline-key autoincrement", function () {
 
 });
 
-asyncTest("Inserting data - offline-key autoincrement", function () {
-  var db = new ydn.db.Storage(db_name + '4', schema);
+asyncTest("Put data - offline-key autoincrement", function () {
+  var db = new ydn.db.Storage(db_name, schema);
   expect(2);
-
 
   db.put(store_name_4, data_1).then(function (x) {
     ok(true, 'no key data insert ok');
@@ -118,6 +140,20 @@ asyncTest("Inserting data - offline-key autoincrement", function () {
       ok(false, e.message);
       start();
     });
+  }, function (e) {
+    ok(false, e.message);
+    start();
+  });
+});
+
+
+asyncTest("Put data with nested", function () {
+  var db = new ydn.db.Storage(db_name, schema);
+  expect(1);
+
+  db.put(store_name_5, gdata_1).then(function (x) {
+    equal(gdata_1.id.$t, x, 'key');
+    start();
   }, function (e) {
     ok(false, e.message);
     start();
