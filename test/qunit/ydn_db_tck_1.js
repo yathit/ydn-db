c5d55ea5cc52d339a4cb9446f5d9d728979c34d6
kyaw <kyaw@blueprint.org>
2013-01-30 17:21:14 +0800

    better async testing design pattern.

--- test/qunit/ydn_db_tck_1.js
+++ test/qunit/ydn_db_tck_1.js
@@ -47,7 +47,7 @@ var schema_1 = {
     {
       name: store_nested_key,
       keyPath: 'id.$t', // gdata style key.
-      type: 'NUMERIC'}
+      type: 'TEXT'}
   ]
 };
 
@@ -94,7 +94,8 @@ var schema_auto_increase = {
     setup: function () {
       test_env.ydnTimeoutId = setTimeout(function () {
         start();
-      }, 2000);
+        console.warn('Put test not finished.');
+      }, 1000);
     },
     teardown: function () {
       clearTimeout(test_env.ydnTimeoutId);
@@ -104,7 +105,6 @@ var schema_auto_increase = {
   };
 
   module("Put", test_env);
-
 
   asyncTest("single data", function () {
     expect(1);
@@ -217,16 +217,26 @@ var schema_auto_increase = {
   var db_name = 'tck1-get-1';
   var data_store_inline = {id: 1, value: 'value ' + Math.random()};
   var data_store_inline_string = {id: 'a', value: 'value ' + Math.random()};
+  var value_store_outline = 'value ' + Math.random();
+  var key_store_outline = Math.random();
+  var value_store_outline_string = 'value ' + Math.random();
+  var key_store_outline_string = 'id' + Math.random();
+  var data_nested_key = { test: "test value", name: "name 3", id: {$t: 'id' + Math.random()} };
 
   var ready = $.Deferred();
 
   // persist store data.
+  // we don't want to share this database connection and test database connection.
   (function() {
     var _db = new ydn.db.Storage(db_name, schema_1);
     _db.put(store_inline, data_store_inline);
+    _db.put(store_outline, {abc: value_store_outline}, key_store_outline);
+    _db.put(store_outline_string, {abc: value_store_outline_string}, key_store_outline_string);
+    _db.put(store_nested_key, data_nested_key);
     _db.put(store_inline_string, data_store_inline_string).always(function() {
       ready.resolve();
     });
+    _db.close();
   })();
 
   var test_env = {
@@ -234,7 +244,8 @@ var schema_auto_increase = {
       db = new ydn.db.Storage(db_name, schema_1);
       test_env.ydnTimeoutId = setTimeout(function () {
         start();
-      }, 2000);
+        console.warn('Get test not finished.');
+      }, 1000);
     },
     teardown: function () {
       clearTimeout(test_env.ydnTimeoutId);
@@ -259,7 +270,6 @@ var schema_auto_increase = {
       });
     });
 
-
   });
 
   asyncTest("inline-line string key", function () {
@@ -274,52 +284,37 @@ var schema_auto_increase = {
 
   });
 
-  asyncTest("outoff-line number key", function () {
-    expect(2);
-    var value_1 = 'test ' + Math.random();
-    var key_in = Math.random();
-    db.put(store_outline, {abc: value_1}, key_in).then(function (key) {
-      equal(key_in, key, 'got same key');
-      db.get(store_outline, key_in).then(function (x) {
-        equal(value_1, x.abc, 'value');
-        start();
-      }, function (e) {
-        ok(false, e.message);
-        start();
-      });
-    }, function (e) {
-      ok(false, e.message);
-      start();
-    });
-
-  });
-
-  asyncTest("outoff-line string key", function () {
-    expect(2);
-    var value_1 = 'test ' + Math.random();
-    var key_in = 'id' + Math.random();
-    db.put(store_outline_string, {abc: value_1}, key_in).then(function (key) {
-      equal(key_in, key, 'got same key');
-      db.get(store_outline_string, key_in).then(function (x) {
-        equal(value_1, x.abc, 'value');
-        start();
-      }, function (e) {
-        ok(false, e.message);
-        start();
-      });
-    }, function (e) {
-      ok(false, e.message);
-      start();
-    });
-
-  });
-
-
-  asyncTest("nested key", function () {
+  asyncTest("out-off-line number key", function () {
     expect(1);
-    db.put(store_nested_key, gdata_1);
-    db.get(store_nested_key, gdata_1.id.$t).then(function (x) {
-      deepEqual(gdata_1, x, 'same object ' + JSON.stringify(x));
+    db.get(store_outline, key_store_outline).then(function (x) {
+      equal(value_store_outline, x && x.abc, 'value');
+      start();
+    }, function (e) {
+      ok(false, e.message);
+      start();
+    });
+
+  });
+
+  asyncTest("out-off-line string key", function () {
+    expect(1);
+
+    db.get(store_outline_string, key_store_outline_string).then(function (x) {
+      equal(value_store_outline_string, x && x.abc, 'value');
+      start();
+    }, function (e) {
+      ok(false, e.message);
+      start();
+    });
+
+  });
+
+
+  asyncTest("nested key path", function () {
+    expect(1);
+
+    db.get(store_nested_key, data_nested_key.id.$t).then(function (x) {
+      deepEqual(data_nested_key, x, 'same object ');
       start();
     }, function (e) {
       ok(false, e.message);
@@ -332,41 +327,65 @@ var schema_auto_increase = {
 
 (function () {
 
+  var db;
+  var db_name = 'tck1-list';
+
   var data_list_inline = [];
   var data_list_outline = [];
   var keys_list_outline = [];
+  for (var i = 0; i < 5; i++) {
+    data_list_inline[i] = {id: i, type: 'inline', value: 'test inline ' + Math.random()};
+    data_list_outline[i] = {type: 'offline', value: 'test out of line ' + Math.random()};
+    keys_list_outline[i] = i;
+  }
 
-  module("List", {
+  var ready = $.Deferred();
+
+  // persist store data.
+  // we don't want to share this database connection and test database connection.
+  (function() {
+    var _db = new ydn.db.Storage(db_name, schema_1);
+    _db.put(store_inline, data_list_inline);
+    _db.put(store_outline, data_list_outline, keys_list_outline).always(function() {
+      ready.resolve();
+    });
+    _db.close();
+  })();
+
+  var test_env = {
     setup: function () {
-      db = new ydn.db.Storage('tck1-list', schema_1);
-      db.clear();
-
-      for (var i = 0; i < 5; i++) {
-        data_list_inline[i] = {id: i, type: 'inline', value: 'test inline ' + Math.random()};
-        data_list_outline[i] = {type: 'offline', value: 'test out of line ' + Math.random()};
-        keys_list_outline[i] = i;
-      }
-      db.put(store_inline, data_list_inline);
-      db.put(store_outline, data_list_outline, keys_list_outline);
-
+      db = new ydn.db.Storage(db_name, schema_1);
+      test_env.ydnTimeoutId = setTimeout(function () {
+        start();
+        console.warn('List test not finished.');
+      }, 1000);
     },
     teardown: function () {
+      clearTimeout(test_env.ydnTimeoutId);
       db.close();
-      //ydn.db.deleteDatabase(db_name_tck1);
+      //ydn.db.deleteDatabase(db.getName());
     }
-  });
+  };
+
+
+  module("List", test_env);
 
 
   asyncTest("Retrieve all objects from a store - inline key", function () {
-    expect(1);
 
-    db.list(store_inline).then(function (x) {
-      deepEqual(data_list_inline, x, 'from 1 to 2');
-      start();
-    }, function (e) {
-      ok(false, e.message);
-      start();
+    ready.always(function() {
+      expect(1);
+      db.list(store_inline).then(function (x) {
+        deepEqual(data_list_inline, x, 'all');
+        start();
+      }, function (e) {
+        ok(false, e.message);
+        start();
+      });
     });
+
+
+
 
   });
 
@@ -486,45 +505,68 @@ var schema_auto_increase = {
 
 (function () {
 
+  var db;
+  var db_name = 'tck1-keys';
 
   var keys_inline = [1, 2, 10, 20, 100];
   var keys_inline_string = ['ab1', 'ab2', 'ac1', 'ac2', 'b'];
+  var inline_data = keys_inline.map(function (x) {
 
-  module("Keys", {
+    return {id: x, value: Math.random()}
+  });
+  var inline_string_data = keys_inline_string.map(function (x) {
+    return {id: x, value: Math.random()}
+  });
+
+  var ready = $.Deferred();
+
+  // persist store data.
+  // we don't want to share this database connection and test database connection.
+  (function() {
+    var _db = new ydn.db.Storage(db_name, schema_1);
+    _db.clear(store_inline);
+    _db.clear(store_inline_string);
+
+    _db.put(store_inline_string, inline_string_data).fail(function (e) {
+      throw e;
+    });
+    //console.log(inline_data);
+    _db.put(store_inline, inline_data).always(function() {
+      ready.resolve();
+    });
+    _db.close();
+  })();
+
+  var test_env = {
     setup: function () {
-      db = new ydn.db.Storage('tck1-keys', schema_1);
-      var inline_data = keys_inline.map(function (x) {
-        return {id: x}
-      });
-      var inline_string_data = keys_inline_string.map(function (x) {
-        return {id: x}
-      });
-      db.put(store_inline_string, inline_string_data).fail(function (e) {
-        throw e;
-      });
-      //console.log(inline_data);
-      db.put(store_inline, inline_data).then(function (keys) {
-        console.log('tck1-keys db ready.');
+      db = new ydn.db.Storage(db_name, schema_1);
+      test_env.ydnTimeoutId = setTimeout(function () {
+        start();
+        console.warn('Keys test not finished.');
+      }, 1000);
+    },
+    teardown: function () {
+      clearTimeout(test_env.ydnTimeoutId);
+      db.close();
+      //ydn.db.deleteDatabase(db.getName());
+    }
+  };
+
+  module("Keys", test_env);
+
+  asyncTest("get integer keys from a store", function () {
+
+    ready.always(function() {
+      expect(1);
+
+      db.keys(store_inline).then(function (keys) {
+        deepEqual(keys_inline, keys, 'key length');
+        start();
       }, function (e) {
         throw e;
       });
-
-    },
-    teardown: function () {
-      db.close();
-      //ydn.db.deleteDatabase(db_name_tck1);
-    }
-  });
-
-  asyncTest("get integer keys from a store", function () {
-    expect(1);
-
-    db.keys(store_inline).then(function (keys) {
-      deepEqual(keys_inline, keys, 'key length');
-      start();
-    }, function (e) {
-      throw e;
     });
+
   });
 
 
@@ -574,38 +616,57 @@ var schema_auto_increase = {
     }, 2000);
   });
 
+})();
 
-  var db_count = 'ydn_db_tck1_count_2';
+(function () {
 
-  module("Count", {
+
+  var db;
+  var db_name ='ydn_db_tck1_count_2';
+
+  var ready = $.Deferred();
+
+  // persist store data.
+  // we don't want to share this database connection and test database connection.
+  (function() {
+    var _db = new ydn.db.Storage(db_name, schema_1);
+    _db.clear();
+    var data = [];
+    var data2 = [];
+    for (var i = 0; i < 5; i++) {
+      data[i] = {id: i, value: 'test' + Math.random()};
+    }
+    var keys = [];
+    for (var i = 0; i < 3; i++) {
+      keys[i] = i;
+      data2[i] = {type: 'offline', value: 'test' + Math.random()};
+    }
+    _db.put(store_outline, data2, keys).fail(function (e) {
+      throw e;
+    });
+    _db.put(store_inline, data).always(function() {
+      ready.resolve();
+    });
+
+    _db.close();
+  })();
+
+  var test_env = {
     setup: function () {
-      db = new ydn.db.Storage(db_count, schema_1);
-      db.clear();
-      var data = [];
-      var data2 = [];
-      for (var i = 0; i < 5; i++) {
-        data[i] = {id: i, value: 'test' + Math.random()};
-      }
-      var keys = [];
-      for (var i = 0; i < 3; i++) {
-        keys[i] = i;
-        data2[i] = {type: 'offline', value: 'test' + Math.random()};
-      }
-      db.put(store_outline, data2, keys).fail(function (e) {
-        throw e;
-      });
-      db.put(store_inline, data).then(function (keys) {
-        console.log('count db ready.')
-      }, function (e) {
-        throw e;
-      });
-
+      db = new ydn.db.Storage(db_name, schema_1);
+      test_env.ydnTimeoutId = setTimeout(function () {
+        start();
+        console.warn('Count test not finished.');
+      }, 1000);
     },
     teardown: function () {
+      clearTimeout(test_env.ydnTimeoutId);
       db.close();
-      //ydn.db.deleteDatabase(db_name_tck1);
+      //ydn.db.deleteDatabase(db.getName());
     }
-  });
+  };
+
+  module("Count", test_env);
 
 
   asyncTest("all records in a store", function () {
