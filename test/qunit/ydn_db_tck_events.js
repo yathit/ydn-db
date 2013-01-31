bdcd65f5a014ceaf0fee3bc2a8b4b53779ed38d2
kyaw <kyaw@blueprint.org>
2013-01-31 14:17:49 +0800

    iterator unit tested.

--- test/qunit/ydn_db_tck_events.js
+++ test/qunit/ydn_db_tck_events.js
@@ -31,165 +31,202 @@ var events_schema = {
       type: 'NUMERIC'}
   ]};
 
-module("Event", {
-  setup: function() {
-
-  },
-  teardown: function() {
-    //db.close();
-    //ydn.db.deleteDatabase(db.getName());
-  }
-});
-
-
-asyncTest("connected", function () {
-  expect(2);
-
-  var db = new ydn.db.Storage(db_name_event, events_schema);
-
-  db.addEventListener('connected', function(e) {
-    equal(e.type, 'connected', 'connected event');
-    ok( e.version > 0, 'version number');
-    start();
-  });
-
-});
-
-
-asyncTest("created RecordEvent", function () {
-  expect(5);
-
-  var db = new ydn.db.Storage(db_name_event, events_schema);
-
-  var key = Math.ceil(Math.random() * 100000);
-  var data = { test:"random value", name:"name " + key, id:key };
-
-  db.addEventListener(['created', 'updated'], function(e) {
-    //console.log(e);
-    equal(e.name, 'RecordEvent', 'event name');
-    equal(e.getStoreName(), store_inline, 'store name');
-    //equal(e.store_name, store_inline, 'store name');
-    equal(e.type, 'created', 'type');
-    equal(e.getKey(), key, 'key');
-    deepEqual(e.getValue(), data, 'value');
-    start();
-  });
-
-  db.add(store_inline, data);
-
-  setTimeout(function() {
-    // don't wait more than 5 sec.
-    start();
-  }, 1000);
-});
-
-
-asyncTest("created StoreEvent", function () {
-  expect(5);
-
-  var db = new ydn.db.Storage(db_name_event, events_schema);
-
-  var keys = [Math.ceil(Math.random() * 100000),
-    Math.ceil(Math.random() * 100000)];
-  var data = [{name:"rand key 1", id: keys[0]}, {name:"rand key 2", id: keys[1]}];
-
-  db.addEventListener(['created', 'updated'], function(e) {
-    // console.log(e);
-    equal(e.name, 'StoreEvent', 'event name');
-    equal(e.getStoreName(), store_inline, 'store name');
-    //equal(e.store_name, store_inline, 'store name');
-    equal(e.type, 'created', 'type');
-    deepEqual(e.getKeys(), keys, 'keys');
-    deepEqual(e.getValues(), data, 'values');
-    start();
-  });
-
-  db.add(store_inline, data);
-  setTimeout(function() {
-    // don't wait more than 5 sec.
-    start();
-  }, 1000);
-
-});
-
-asyncTest("updated RecordEvent", function () {
-  expect(10);
-
-  var db = new ydn.db.Storage(db_name_event, events_schema);
-
-  var key = Math.ceil(Math.random() * 100000);
-  var data = { test:"random value", name:"name " + key, id:key };
-
-  var firedCreatedEvent = false;
-  db.addEventListener(['created', 'updated'], function(e) {
-    //console.log(e);
-    if (!firedCreatedEvent) {
-      equal(e.name, 'RecordEvent', 'event name');
-      equal(e.getStoreName(), store_inline, 'store name');
-      //equal(e.store_name, store_inline, 'store name');
-      equal(e.type, 'created', 'type');
-      equal(e.getKey(), key, 'key');
-      deepEqual(e.getValue(), data, 'value');
-      firedCreatedEvent = true;
-    } else {
-      equal(e.name, 'RecordEvent', 'event name');
-      equal(e.getStoreName(), store_inline, 'store name');
-      //equal(e.store_name, store_inline, 'store name');
-      equal(e.type, 'updated', 'type');
-      equal(e.getKey(), key, 'key');
-      deepEqual(e.getValue(), data, 'value');
+(function(){
+  var test_env = {
+    setup: function () {
+      test_env.ydnTimeoutId = setTimeout(function () {
+        start();
+        console.warn('Storage Event test not finished.');
+      }, 1000);
+    },
+    teardown: function () {
+      clearTimeout(test_env.ydnTimeoutId);
     }
-    start();
-  });
+  };
 
-  db.add(store_inline, data);
-  db.put(store_inline, data);
-  setTimeout(function() {
-    // don't wait more than 5 sec.
-    start();
-  }, 2000);
-});
+  module("Storage Event", test_env);
 
 
-asyncTest("updated StoreEvent", function () {
-  expect(10);
+  asyncTest("connected", function () {
+    expect(2);
 
-  var db = new ydn.db.Storage(db_name_event, events_schema);
-  var keys = [Math.ceil(Math.random() * 100000),
-    Math.ceil(Math.random() * 100000)];
-  var data = [{name:"rand key 1", id: keys[0]}, {name:"rand key 2", id: keys[1]}];
+    var db = new ydn.db.Storage(db_name_event, events_schema);
 
-  var firedCreatedEvent = false;
-  db.addEventListener(['created', 'updated'], function(e) {
-    //console.log(e);
-    if (!firedCreatedEvent) {
-      equal(e.name, 'StoreEvent', 'event name');
-      equal(e.getStoreName(), store_inline, 'store name');
-      //equal(e.store_name, store_inline, 'store name');
-      equal(e.type, 'created', 'type');
-      deepEqual(e.getKeys(), keys, 'created key');
-      deepEqual(e.getValues(), data, 'created value');
-      firedCreatedEvent = true;
-    } else {
-      equal(e.name, 'StoreEvent', 'event name');
-      equal(e.getStoreName(), store_inline, 'store name');
-      //equal(e.store_name, store_inline, 'store name');
-      equal(e.type, 'updated', 'type');
-      deepEqual(e.getKeys(), keys, 'updated key');
-      deepEqual(e.getValues(), data, 'updated value');
-
+    db.addEventListener('connected', function(e) {
+      equal(e.type, 'connected', 'connected event');
+      ok( e.version > 0, 'version number');
+      db.close();
       start();
-    }
+    });
+
   });
 
-  db.add(store_inline, data);
-  db.put(store_inline, data);
-  setTimeout(function() {
-    // don't wait more than 5 sec.
-    start();
-  }, 1000);
-});
+})();
 
+
+(function () {
+  var test_env = {
+    setup: function () {
+      test_env.ydnTimeoutId = setTimeout(function () {
+        start();
+        console.warn('RecordEvent Event test not finished.');
+      }, 1000);
+    },
+    teardown: function () {
+      clearTimeout(test_env.ydnTimeoutId);
+    }
+  };
+
+  module("RecordEvent Event", test_env);
+
+  asyncTest("created", function () {
+    expect(5);
+
+    var db = new ydn.db.Storage(db_name_event, events_schema);
+
+    var key = Math.ceil(Math.random() * 100000);
+    var data = { test: "random value", name: "name " + key, id: key };
+
+    db.addEventListener(['created', 'updated'], function (e) {
+      //console.log(e);
+      equal(e.name, 'RecordEvent', 'event name');
+      equal(e.getStoreName(), store_inline, 'store name');
+      //equal(e.store_name, store_inline, 'store name');
+      equal(e.type, 'created', 'type');
+      equal(e.getKey(), key, 'key');
+      deepEqual(e.getValue(), data, 'value');
+      start();
+    });
+
+    db.add(store_inline, data);
+
+  });
+
+
+  asyncTest("updated", function () {
+    expect(10);
+
+    var db = new ydn.db.Storage(db_name_event, events_schema);
+
+    var key = Math.ceil(Math.random() * 100000);
+    var data = { test: "random value", name: "name " + key, id: key };
+
+    var firedCreatedEvent = false;
+    db.addEventListener(['created', 'updated'], function (e) {
+      //console.log(e);
+      if (!firedCreatedEvent) {
+        equal(e.name, 'RecordEvent', 'event name');
+        equal(e.getStoreName(), store_inline, 'store name');
+        //equal(e.store_name, store_inline, 'store name');
+        equal(e.type, 'created', 'type');
+        equal(e.getKey(), key, 'key');
+        deepEqual(e.getValue(), data, 'value');
+        firedCreatedEvent = true;
+      } else {
+        equal(e.name, 'RecordEvent', 'event name');
+        equal(e.getStoreName(), store_inline, 'store name');
+        //equal(e.store_name, store_inline, 'store name');
+        equal(e.type, 'updated', 'type');
+        equal(e.getKey(), key, 'key');
+        deepEqual(e.getValue(), data, 'value');
+
+        start();
+      }
+    });
+
+    db.add(store_inline, data);
+    db.put(store_inline, data);
+
+  });
+
+
+})();
+
+
+(function () {
+  var test_env = {
+    setup: function () {
+      test_env.ydnTimeoutId = setTimeout(function () {
+        start();
+        console.warn('Store Event test not finished.');
+      }, 1000);
+    },
+    teardown: function () {
+      clearTimeout(test_env.ydnTimeoutId);
+    }
+  };
+
+  module("Store Event", test_env);
+
+  asyncTest("created", function () {
+    expect(5);
+
+    var db = new ydn.db.Storage(db_name_event, events_schema);
+
+    var keys = [Math.ceil(Math.random() * 100000),
+      Math.ceil(Math.random() * 100000)];
+    var data = [
+      {name: "rand key 1", id: keys[0]},
+      {name: "rand key 2", id: keys[1]}
+    ];
+
+    db.addEventListener(['created', 'updated'], function (e) {
+      // console.log(e);
+      equal(e.name, 'StoreEvent', 'event name');
+      equal(e.getStoreName(), store_inline, 'store name');
+      //equal(e.store_name, store_inline, 'store name');
+      equal(e.type, 'created', 'type');
+      deepEqual(e.getKeys(), keys, 'keys');
+      deepEqual(e.getValues(), data, 'values');
+      start();
+    });
+
+    db.add(store_inline, data);
+
+  });
+
+
+  asyncTest("updated", function () {
+    expect(10);
+
+    var db = new ydn.db.Storage(db_name_event, events_schema);
+    var keys = [Math.ceil(Math.random() * 100000),
+      Math.ceil(Math.random() * 100000)];
+    var data = [
+      {name: "rand key 1", id: keys[0]},
+      {name: "rand key 2", id: keys[1]}
+    ];
+
+    var firedCreatedEvent = false;
+    db.addEventListener(['created', 'updated'], function (e) {
+      //console.log(e);
+      if (!firedCreatedEvent) {
+        equal(e.name, 'StoreEvent', 'event name');
+        equal(e.getStoreName(), store_inline, 'store name');
+        //equal(e.store_name, store_inline, 'store name');
+        equal(e.type, 'created', 'type');
+        deepEqual(e.getKeys(), keys, 'created key');
+        deepEqual(e.getValues(), data, 'created value');
+        firedCreatedEvent = true;
+      } else {
+        equal(e.name, 'StoreEvent', 'event name');
+        equal(e.getStoreName(), store_inline, 'store name');
+        //equal(e.store_name, store_inline, 'store name');
+        equal(e.type, 'updated', 'type');
+        deepEqual(e.getKeys(), keys, 'updated key');
+        deepEqual(e.getValues(), data, 'updated value');
+
+        start();
+      }
+    });
+
+    db.add(store_inline, data);
+    db.put(store_inline, data);
+
+  });
+
+
+})();
 
 
 
