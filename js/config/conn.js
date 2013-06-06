361dbfa47b5e73bd92b8bfc09163c3da78cc1133
kyaw <kyaw@blueprint.org>
2013-06-06 09:21:00 +0800

    externs and exports

--- js/config/conn.js
+++ js/config/conn.js
@@ -2,10 +2,10 @@
  * @fileoverview Exports for conn-ydn-db module.
  */
 
-goog.require('ydn.db');
-goog.require('ydn.db.con.Storage');
 goog.require('goog.async.Deferred');
 goog.require('ydn.base');
+goog.require('ydn.db');
+goog.require('ydn.db.con.Storage');
 goog.require('ydn.db.events.StorageEvent');
 
 goog.exportSymbol('ydn.db.con.Storage', ydn.db.con.Storage);
@@ -14,17 +14,17 @@ goog.exportSymbol('ydn.db.con.Storage', ydn.db.con.Storage);
 // goog.exportProperty(ydn.db.con.Storage.prototype, 'onReady',
 //  ydn.db.con.Storage.prototype.onReady);
 goog.exportProperty(ydn.db.con.Storage.prototype, 'getType',
-  ydn.db.con.Storage.prototype.getType);
+    ydn.db.con.Storage.prototype.getType);
 goog.exportProperty(ydn.db.con.Storage.prototype, 'setName',
-  ydn.db.con.Storage.prototype.setName);
+    ydn.db.con.Storage.prototype.setName);
 goog.exportProperty(ydn.db.con.Storage.prototype, 'getName',
     ydn.db.con.Storage.prototype.getName);
 goog.exportProperty(ydn.db.con.Storage.prototype, 'getSchema',
-  ydn.db.con.Storage.prototype.getSchema);
+    ydn.db.con.Storage.prototype.getSchema);
 goog.exportProperty(ydn.db.con.Storage.prototype, 'transaction',
-  ydn.db.con.Storage.prototype.transaction);
+    ydn.db.con.Storage.prototype.transaction);
 goog.exportProperty(ydn.db.con.Storage.prototype, 'close',
-  ydn.db.con.Storage.prototype.close);
+    ydn.db.con.Storage.prototype.close);
 
 // for hacker only. This method should not document this, since this will change
 // transaction state.
