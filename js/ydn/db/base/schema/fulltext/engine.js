5de896a19d85381ea136245dd1ce1e57e3d73769
kyawtun <okisan.2008@gmail.com>
2013-07-30 16:38:26 +0800

    compiled OK.

--- js/ydn/db/base/schema/fulltext/engine.js
+++ js/ydn/db/base/schema/fulltext/engine.js
@@ -52,11 +52,9 @@ ydn.db.schema.fulltext.Engine = function() {};
 /**
  * @param {string} text text to be prase and scored.
  * @param {ydn.db.schema.FullTextSource} source
- * @param {IDBKey=} opt_key primary key.
  * @return {Array.<ydn.db.schema.fulltext.ScoreEntry>} scores for each unique token.
  */
-ydn.db.schema.fulltext.Engine.prototype.score =
-    function(text, source, opt_key) {};
+ydn.db.schema.fulltext.Engine.prototype.score =function(text, source) {};
 
 
 /**
