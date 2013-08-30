b7292cdcba32690507e43f91984f30d0b1c54ad3
kyaw <kyaw@blueprint.org>
2013-08-30 16:10:26 +0800

    clean exports

--- js/config/sql.js
+++ js/config/sql.js
@@ -8,5 +8,6 @@ goog.require('ydn.db.con.exports');
 goog.require('ydn.db.core.exports');
 goog.require('ydn.db.crud.exports');
 goog.require('ydn.db.sql.Storage');
+goog.require('ydn.db.sql.exports');
 
 goog.exportSymbol('ydn.db.Storage', ydn.db.sql.Storage);
--- js/config/storage.js
+++ js/config/storage.js
@@ -8,6 +8,7 @@ goog.require('ydn.db.Storage');
 goog.require('ydn.db.con.exports');
 goog.require('ydn.db.core.exports');
 goog.require('ydn.db.crud.exports');
+goog.require('ydn.db.sql.exports');
 
 goog.exportSymbol('ydn.db.Storage', ydn.db.Storage);
 
