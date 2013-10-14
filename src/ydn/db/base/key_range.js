442c0e8dcbee09a63027a5d1369922c72911f45d
kyawtun <okisan.2008@gmail.com>
2013-10-15 00:37:49 +0800

    refactored with new build system.

--- src/ydn/db/base/key_range.js
+++ src/ydn/db/base/key_range.js
@@ -186,10 +186,90 @@ ydn.db.KeyRange.starts = function(value) {
   } else if (goog.isString(value)) {
     value_upper = value + '\uffff';
   } else {
-    throw new ydn.debug.error.ArgumentException();
+    return ydn.db.KeyRange.only(value);
   }
 
   return ydn.db.KeyRange.bound(value, value_upper, false, true);
+};
+
+
+/**
+ * This defines no successor of empty arrays, so the range for prefix
+ * [] or [[],[]] has no upper bound.
+ * An alternate definition would preclude adding additional nesting,
+ * so the range for prefix [] would have upper bound [[]] and the
+ * range for prefix [[], []] would have upper bound [[], [[]]].
+ * {@link https://gist.github.com/inexorabletash/5462871}
+ * @param {IDBKey} prefix
+ * @return {IDBKeyRange}
+ */
+ydn.db.KeyRange.forPrefix = function(prefix) {
+  // Ensure prefix is a valid key itself:
+  if (indexedDB.cmp(prefix, prefix) !== 0) throw new TypeError();
+
+  var MAX_DATE_VALUE = 8640000000000000;
+  var UPPER_BOUND = {
+    NUMBER: new Date(-MAX_DATE_VALUE),
+    DATE: "",
+    STRING: [],
+    ARRAY: undefined
+  };
+
+  var upperKey = successor(prefix);
+  if (upperKey === undefined)
+    return IDBKeyRange.lowerBound(prefix);
+  return IDBKeyRange.bound(prefix, upperKey, false, true);
+
+  function successor(key) {
+
+    if (typeof key === 'number') {
+      if (key === Infinity)
+        return UPPER_BOUND.NUMBER;
+      if (key === -Infinity)
+        return -Number.MAX_VALUE;
+      if (key === 0)
+        return Number.MIN_VALUE;
+      var epsilon = Math.abs(key);
+      while (key + epsilon / 2 !== key)
+        epsilon = epsilon / 2;
+      return key + epsilon;
+    }
+
+    if (key instanceof Date) {
+      if (key.valueOf() + 1 > MAX_DATE_VALUE)
+        return UPPER_BOUND.DATE;
+      return new Date(key.valueOf() + 1);
+    }
+
+    if (typeof key === 'string') {
+      var len = key.length;
+      while (len > 0) {
+        var head = key.substring(0, len - 1),
+            tail = key.charCodeAt(len - 1);
+        if (tail !== 0xFFFF)
+          return head + String.fromCharCode(tail + 1);
+        key = head;
+        --len;
+      }
+      return UPPER_BOUND.STRING;
+    }
+
+    if (Array.isArray(key)) {
+      key = key.slice();  // Operate on a copy.
+      len = key.length;
+      while (len > 0) {
+        tail = successor(key.pop());
+        if (tail !== undefined) {
+          key.push(tail);
+          return key;
+        }
+        --len;
+      }
+      return UPPER_BOUND.ARRAY;
+    }
+
+    throw new TypeError();
+  }
 };
 
 
