b7b9a68dac1a17af8aa7acf67f654cfd8f96bc20
kyawtun <okisan.2008@gmail.com>
2013-12-22 16:22:54 +0800

    remove unnecessary require

--- src/ydn/db/base/utils.js
+++ src/ydn/db/base/utils.js
@@ -1,9 +1,23 @@
+// Copyright 2012 YDN Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /**
- * @fileoverview About this file.
+ * @fileoverview Utilities for encoding IDBKey and key comparison algorithm.
  *
- * User: kyawtun
- * Date: 21/10/12
+ * @author kyawtun@yathit.com (Kyaw Tun)
  */
+
 
 /**
  * Portion of this code is obtained from Facebook Inc's IndexedDB-polyfill
