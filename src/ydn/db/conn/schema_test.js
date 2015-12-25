6d631b73df0183487edae1dca78399bfe2b1b596
Kyaw Tun <kyawtun@yathit.com>
2015-12-25 09:54:51 +0800

    migrate to new testing framework.

--- src/ydn/db/conn/schema_test.js
+++ src/ydn/db/conn/schema_test.js
@@ -1,28 +1,35 @@
 
+goog.provide('ydn.db.con.SchemaTest');
+goog.setTestOnly('ydn.db.con.SchemaTest');
+
 goog.require('goog.debug.Console');
-goog.require('goog.testing.PropertyReplacer');
+goog.require('goog.events.EventTarget');
+goog.require('goog.testing.AsyncTestCase');
+goog.require('goog.testing.asserts');
 goog.require('goog.testing.jsunit');
 goog.require('ydn.db');
 goog.require('ydn.db.crud.Storage');
 goog.require('ydn.debug');
 
 
-var reachedFinalContinuation, schema, debug_console;
+var schema, debug_console;
+
+var asyncTestCase = goog.testing.AsyncTestCase.createAndInstall();
 
 
 
-var setUp = function() {
+
+function setUp() {
   // ydn.debug.log('ydn.db.con', 'finest');
   // ydn.db.con.IndexedDb.DEBUG = true;
-  reachedFinalContinuation = false;
 
-};
+}
 
-var tearDown = function() {
-  assertTrue('The final continuation was not reached', reachedFinalContinuation);
-};
+function tearDown() {
 
-var test_auto_schema = function() {
+}
+
+function test_auto_schema() {
 
   var db_name = 'test_' + Math.random();
   // autoSchema database
@@ -33,93 +40,71 @@ var test_auto_schema = function() {
   var table_name = 'st1';
   var store_schema = {'name': table_name, 'keyPath': 'id', 'type': 'TEXT'};
 
-  var hasEventFired = false;
-  var result;
   var value = 'a' + Math.random();
-
-  waitForCondition(
-    // Condition
-    function() { return hasEventFired; },
-    // Continuation
-    function() {
+  asyncTestCase.waitForAsync('put');
+  db.put(store_schema, {
+    id: 'a',
+    value: value,
+    remark: 'put test'
+  }).addCallback(function(y) {
+    // console.log('put key ' + y);
+    db.get(table_name, 'a').addCallback(function(x) {
+      var result = x.value;
       assertEquals('get back', value, result);
-      // Remember, the state of this boolean will be tested in tearDown().
-      reachedFinalContinuation = true;
+      asyncTestCase.continueTesting();
       ydn.db.deleteDatabase(db.getName(), db.getType());
       db.close();
-    },
-    100, // interval
-    2000); // maxTimeout
-
-  db.put(store_schema, {id: 'a', value: value, remark: 'put test'}).addCallback(function(y) {
-    // console.log('put key ' + y);
-
-    db.get(table_name, 'a').addCallback(function(x) {
-      result = x.value;
-      hasEventFired = true;
     });
   }).addErrback(function(e) {
-      hasEventFired = true;
-      console.log('Error: ' + e);
-    });
-};
-
-var version_change_test_count = 0;
+    fail(e);
+    asyncTestCase.continueTesting();
+    ydn.db.deleteDatabase(db.getName(), db.getType());
+    db.close();
+  });
+}
 
 var version_change_test = function(schema1, schema2, is_final, msg, hint_sql, hint_idb) {
   var db_name = 'test' + Math.random();
   msg = msg || '';
 
-  var ver, oldVer, ver2, oldVer2, ex_schema1, ex_schema2;
-  var done = false;
-
-  waitForCondition(
-      // Condition
-      function() { return done; },
-      // Continuation
-      function() {
-        assertNotNaN(msg + 'change_test version 1', ver);
-        assertNaN(msg + 'change_test old version 1', oldVer);
-        assertEquals(msg + 'change_test version 2', (ver + 1), ver2);
-        assertEquals(msg + 'change_test old version 2', ver, oldVer2);
-        var s1 = new ydn.db.schema.Database(schema1);
-        var msg1 = s1.difference(ex_schema1, !!hint_sql, !!hint_idb);
-        assertTrue(msg + ' schema 1 ' + msg1, !msg1);
-        //console.log(schema2);
-        //console.log(ex_schema2);
-        var s2 = new ydn.db.schema.Database(schema2);
-        var msg2 = s2.difference(ex_schema2);
-        assertTrue(msg + ' schema 2 ' + msg2, !msg2);
-        if (is_final) {
-          reachedFinalContinuation = true;
-        }
-
-      },
-      100, // interval
-      3000); // maxTimeout
-
-
   var db = new ydn.db.crud.Storage(db_name, schema1, options);
+  asyncTestCase.waitForAsync('database ready');
   db.addEventListener('ready', function(e) {
-    ver = e.getVersion();
-    oldVer = e.getOldVersion();
+    var ver = e.getVersion();
+    var oldVer = e.getOldVersion();
     // console.log(ver, oldVer);
     db.getSchema(function(x) {
-      ex_schema1 = new ydn.db.schema.Database(x);
+      var ex_schema1 = new ydn.db.schema.Database(x);
       // console.log(ex_schema1);
       db.close();
+      asyncTestCase.continueTesting();
+      asyncTestCase.waitForAsync('database ready 2');
       setTimeout(function() {
         var db2 = new ydn.db.crud.Storage(db_name, schema2, options);
         db2.addEventListener('ready', function(e) {
-          ver2 = e.getVersion();
-          oldVer2 = e.getOldVersion();
+          var ver2 = e.getVersion();
+          var oldVer2 = e.getOldVersion();
           // console.log(ver2, oldVer2);
           db2.getSchema(function(x) {
-            ex_schema2 = new ydn.db.schema.Database(x);
+            var ex_schema2 = new ydn.db.schema.Database(x);
             // console.log(ex_schema2);
+
+            assertNotNaN(msg + 'change_test version 1', ver);
+            assertNaN(msg + 'change_test old version 1', oldVer);
+            assertEquals(msg + 'change_test version 2', (ver + 1), ver2);
+            assertEquals(msg + 'change_test old version 2', ver, oldVer2);
+            var s1 = new ydn.db.schema.Database(schema1);
+            var msg1 = s1.difference(ex_schema1, !!hint_sql, !!hint_idb);
+            assertTrue(msg + ' schema 1 ' + msg1, !msg1);
+            //console.log(schema2);
+            //console.log(ex_schema2);
+            var s2 = new ydn.db.schema.Database(schema2);
+            var msg2 = s2.difference(ex_schema2);
+            assertTrue(msg + ' schema 2 ' + msg2, !msg2);
+
             ydn.db.deleteDatabase(db2.getName(), db2.getType());
             db2.close();
-            done = true;
+            asyncTestCase.continueTesting();
           });
         });
       }, 200);
@@ -128,7 +113,7 @@ var version_change_test = function(schema1, schema2, is_final, msg, hint_sql, hi
 };
 
 
-var test_add_store = function() {
+function test_add_store() {
   var schema1 = {
     stores: [
       {
@@ -147,9 +132,9 @@ var test_add_store = function() {
     ]
   };
   version_change_test(schema1, schema2, true);
-};
+}
 
-var test_remove_store = function() {
+function test_remove_store() {
   var schema2 = {
     stores: [
       {
@@ -168,9 +153,9 @@ var test_remove_store = function() {
     ]
   };
   version_change_test(schema1, schema2, true);
-};
+}
 
-var test_rename_store = function() {
+function test_rename_store() {
   var schema2 = {
     stores: [
       {
@@ -186,9 +171,9 @@ var test_rename_store = function() {
     ]
   };
   version_change_test(schema1, schema2, true);
-};
+}
 
-var test_out_of_line_to_in_line_key = function() {
+function test_out_of_line_to_in_line_key() {
   var schema1 = {
     stores: [
       {
@@ -205,53 +190,37 @@ var test_out_of_line_to_in_line_key = function() {
     ]
   };
   version_change_test(schema1, schema2, true);
-};
+}
 
 var version_unchange_test = function(schema, is_final, msg) {
   var db_name = 'test' + Math.random();
   msg = msg || '';
 
-  var ver, oldVer, ver2, oldVer2;
-  var done = false;
-
-  waitForCondition(
-      // Condition
-      function() { return done; },
-      // Continuation
-      function() {
-        // console.log([ver, oldVer, ver2, oldVer2]);
-        assertNotNaN(msg + 'unchange_test version 1', ver);
-        assertNaN(msg + 'unchange_test old version 1', oldVer);
-        assertEquals(msg + 'unchange_test version 2, no change', ver, ver2);
-        assertEquals(msg + 'unchange_test old version 2, no change', ver, oldVer2);
-
-        if (is_final) {
-          reachedFinalContinuation = true;
-        }
-
-      },
-      100, // interval
-      2000); // maxTimeout
-
+  asyncTestCase.waitForAsync('database ready');
   var db = new ydn.db.crud.Storage(db_name, schema, options);
   db.addEventListener('ready', function(e) {
-    ver = e.getVersion();
-    oldVer = e.getOldVersion();
+    var ver = e.getVersion();
+    var oldVer = e.getOldVersion();
     db.close();
     setTimeout(function() {
       var db2 = new ydn.db.crud.Storage(db_name, schema, options);
       db2.addEventListener('ready', function(e) {
-        ver2 = e.getVersion();
-        oldVer2 = e.getOldVersion();
+        var ver2 = e.getVersion();
+        var oldVer2 = e.getOldVersion();
+
+        assertNaN(msg + 'unchange_test old version 1', oldVer);
+        assertEquals(msg + 'unchange_test version 2, no change', ver, ver2);
+        assertEquals(msg + 'unchange_test old version 2, no change', ver, oldVer2);
+
         ydn.db.deleteDatabase(db2.getName(), db2.getType());
         db2.close();
-        done = true;
+        asyncTestCase.continueTesting();
       });
     }, 200);
   });
 };
 
-var test_keyPath = function() {
+function test_keyPath() {
 
   var schema = {
     stores: [
@@ -273,9 +242,9 @@ var test_keyPath = function() {
   version_unchange_test(schema2);
   version_change_test(schema, schema2);
   version_change_test(schema2, schema, true);
-};
+}
 
-var test_multiEntry = function() {
+function test_multiEntry() {
 
   var schema = {
     stores: [
@@ -306,9 +275,9 @@ var test_multiEntry = function() {
 
   //version_unchange_test(schema, false, 'multiEntry=false:');
   version_unchange_test(schema2, true, 'multiEntry=true:');
-};
+}
 
-var test_key_path_index = function() {
+function test_key_path_index() {
   var schema = {
     stores: [
       {
@@ -334,10 +303,10 @@ var test_key_path_index = function() {
   version_unchange_test(schema2, false, '2:');
   version_change_test(schema, schema2, false, '3:');
   version_change_test(schema2, schema, true, '4:');
-};
+}
 
 
-var test_auto_increment = function() {
+function test_auto_increment() {
   var schema = {
     stores: [
       {
@@ -362,9 +331,9 @@ var test_auto_increment = function() {
   version_unchange_test(schema2, false, '2:');
   version_change_test(schema, schema2, false, '3:');
   version_change_test(schema2, schema, true, '4:');
-};
+}
 
-var test_composite_key_schema = function() {
+function test_composite_key_schema() {
 
   var schema = {
     stores: [
@@ -389,10 +358,10 @@ var test_composite_key_schema = function() {
   version_unchange_test(schema2, false, '2:');
   version_change_test(schema, schema2, false, '3:');
   version_change_test(schema2, schema, true, '4:');
-};
+}
 
 
-var test_composite_index_schema = function() {
+function test_composite_index_schema() {
 
   if (options.mechanisms[0] == 'websql') {
     // fixme: known issue
@@ -425,10 +394,10 @@ var test_composite_index_schema = function() {
   version_change_test(schema, schema2, false, '3:');
   version_change_test(schema2, schema, true, '4:');
 
-};
+}
 
 
-var test_blob_column = function() {
+function test_blob_column() {
 
   var schema = {
     stores: [{
@@ -457,9 +426,9 @@ var test_blob_column = function() {
   }
   version_change_test(schema2, schema, true, '4:');
 
-};
+}
 
-var test_data_index_add = function() {
+function test_data_index_add() {
   if (options.mechanisms[0] != 'indexeddb') {
     reachedFinalContinuation = true;
     return;
@@ -482,23 +451,6 @@ var test_data_index_add = function() {
     id: 3,
     value: 2
   }];
-  var keys, done;
-
-  waitForCondition(
-      // Condition
-      function() { return done; },
-      // Continuation
-      function() {
-        // console.log([ver, oldVer, ver2, oldVer2]);
-        assertArrayEquals(keys, [1, 3]);
-
-        reachedFinalContinuation = true;
-        ydn.db.deleteDatabase(db.getName(), db.getType());
-        db.close();
-
-      },
-      100, // interval
-      5000); // maxTimeout
 
   var db = new ydn.db.crud.Storage(db_name, schema, options);
   db.clear('st');
@@ -513,18 +465,21 @@ var test_data_index_add = function() {
       }]
     }]
   };
+  asyncTestCase.waitForAsync('database opening');
   setTimeout(function() {
     // make time to close the database, before schema change.
     var db2 = new ydn.db.crud.Storage(db_name, schema2, options);
-    db2.keys('st', 'value', ydn.db.KeyRange.only(2)).addBoth(function(x) {
-      keys = x;
-      done = true;
+    db2.keys('st', 'value', ydn.db.KeyRange.only(2)).addBoth(function(keys) {
+      assertArrayEquals(keys, [1, 3]);
+      ydn.db.deleteDatabase(db.getName(), db.getType());
+      db.close();
+      asyncTestCase.continueTesting();
     });
   }, 1000);
 
-};
+}
 
-var test_mutli_connection = function() {
+function test_multi_connection() {
   if (options.mechanisms[0] != 'indexeddb') {
     reachedFinalContinuation = true;
     return;
@@ -547,27 +502,7 @@ var test_mutli_connection = function() {
     id: 3,
     value: 2
   }];
-  var keys, done, event_vc, event_fail;
-
-  waitForCondition(
-      // Condition
-      function() { return done; },
-      // Continuation
-      function() {
-        // console.log([ver, oldVer, ver2, oldVer2]);
-        assertNotNullNorUndefined('version change event called', event_vc);
-        assertNotNullNorUndefined('fail event called', event_fail);
-        var e = event_fail.getError();
-        assertEquals('fail event name', 'versionchange', e.name);
-        assertArrayEquals(keys, [1, 3]);
-
-        reachedFinalContinuation = true;
-        ydn.db.deleteDatabase(db_name, options.mechanisms[0]);
-        db.close();
-
-      },
-      100, // interval
-      5000); // maxTimeout
+  var event_vc, event_fail;
 
   var db = new ydn.db.crud.Storage(db_name, schema, options);
   db.clear('st');
@@ -590,20 +525,27 @@ var test_mutli_connection = function() {
     // console.log(e);
     event_fail = e;
   });
+
+  asyncTestCase.waitForAsync('database opening');
   setTimeout(function() {
     // make time.
     var db2 = new ydn.db.crud.Storage(db_name, schema2, options);
-    db2.keys('st', 'value', ydn.db.KeyRange.only(2)).addBoth(function(x) {
-      keys = x;
-      done = true;
+    db2.keys('st', 'value', ydn.db.KeyRange.only(2)).addBoth(function(keys) {
+
+      assertNotNullNorUndefined('version change event called', event_vc);
+      assertNotNullNorUndefined('fail event called', event_fail);
+      var e = event_fail.getError();
+      assertEquals('fail event name', 'versionchange', e.name);
+      assertArrayEquals(keys, [1, 3]);
+
+      ydn.db.deleteDatabase(db_name, options.mechanisms[0]);
+      db.close();
+      asyncTestCase.continueTesting();
     });
   }, 1000);
-};
+}
 
 
-var testCase = new goog.testing.ContinuationTestCase();
-testCase.autoDiscoverTests();
-G_testRunner.initialize(testCase);
 
 
 
