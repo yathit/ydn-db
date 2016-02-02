f0573b92950aff203da8648075a1deefe37da8f6
Kyaw Tun <kyawtun@yathit.com>
2016-02-02 22:23:09 +0800

    unit test for issue #17

--- src/ydn/db/query/basic-test.js
+++ src/ydn/db/query/basic-test.js
@@ -86,6 +86,64 @@ function testEquiRestrictionWithOrdering() {
 }
 
 
+
+function testCompoundIndexQuery() {
+
+  var schema = {
+    stores: [
+      {
+        name: 'users',
+        autoIncrement: true,
+        indexes: [
+          {
+            name: 'name',
+            keyPath: 'name'
+          },
+          {
+            name: 'age',
+            keyPath: 'age'
+          },
+          {
+            name: 'name, age',
+            keyPath: ['name', 'age']
+          }
+        ]
+      }
+    ]
+  };
+
+  var values = [
+    {"name": "Lara", "age": 45, "registered": "2015-12-21T10:14:59.661Z"}
+    , {"name": "Sarah", "age": 25, "registered": "2016-01-12T09:00:32.736Z"}
+    , {"name": "Benny", "age": 28, "registered": "2015-12-21T09:55:17.201Z"}
+    , {"name": "John", "age": 39, "registered": "2015-12-18T11:14:50.426Z"}
+    , {"name": "Benny", "age": 30, "registered": "2014-12-18T11:14:50.426Z"}
+  ];
+
+
+  var db_name = 'testCompoundIndexQuery';
+  var db = new ydn.db.Storage(db_name, schema);
+  asyncTestCase.waitForAsync('database ready');
+  db.onReady(function(e) {
+    db.clear();
+    db.put('users', values);
+
+    var query = db.from('users')
+        .where('name, age', '>', ['Benny'], '<', ['Benny', 30])
+        .list()
+        .addCallbacks(function(x) {
+          assertArrayEquals('result', [values[2]], x);
+          asyncTestCase.continueTesting();
+          ydn.db.deleteDatabase(db.getName(), db.getType());
+          db.close();
+        }, function(e) {
+          window.console.error(e);
+        });
+  });
+
+}
+
+
 function testOpen() {
   var schema = {
     stores: [
